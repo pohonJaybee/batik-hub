9b6ad52430e22f8bc97e57e0dcf74aa04003d5bf
Diamond Patrick <diamondpaatrick@gmail.com>
2026-06-04 03:02:29 +0700

    Create service layer for Staff entity

---
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Service/Staff/StaffService.java
@@ -0,0 +1,22 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa.Service.Staff;
+
+import com.ecommerce.web.jpa.e_commerce_web_jpa.Dto.Staff.StaffRequestDTO;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.Dto.Staff.StaffUpdateDTO;
+import com.ecommerce.web.jpa.e_commerce_web_jpa.Entities.Staff;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+
+public interface StaffService {
+
+    void insert(@Valid StaffRequestDTO staffReq);
+
+    Staff findByEmailAndPassword(@NotBlank String email, @NotBlank String password);
+
+    Staff findById(@NotBlank String idStaff);
+
+    Staff update(@NotBlank String id, @Valid StaffUpdateDTO staffReq);
+
+    void delete(@Valid String id);
+
+}
