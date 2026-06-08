384d4f3e6d566e5bad0e413c039f587513300b20
Diamond Patrick <diamondpaatrick@gmail.com>
2026-06-09 04:07:24 +0700

    Testing Controller for Member entity

---
+++ src/test/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Controller/Member/MemberControllerTest.java
@@ -0,0 +1,51 @@
+package com.ecommerce.web.jpa.e_commerce_web_jpa.Controller.Member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testPromoPage() throws Exception {
+        mockMvc.perform(
+                get("/promo")).andExpect(status().isOk());
+    }
+
+    @Test
+    void testCariPage() throws Exception {
+        mockMvc.perform(get("/findproduct")).andExpect(status().isOk());
+    }
+
+    @Test
+    void testCariProduk() throws Exception {
+        mockMvc.perform(
+                post("/findbyname")
+                        .param("produkName", "Kemeja"))
+                .andExpectAll(
+                        status().isOk(),
+                        view().name("visitor/cariPage"),
+                        model().attribute("totalProduk", 1));
+    }
+
+    @Test
+    void testCariProdukZeroResult() throws Exception {
+        mockMvc.perform(
+                post("/findbyname")
+                        .param("produkName", "jkhxsna"))
+                .andExpectAll(
+                        status().isOk(),
+                        view().name("visitor/cariPage"),
+                        model().attribute("totalProduk", 0));
+    }
+}
