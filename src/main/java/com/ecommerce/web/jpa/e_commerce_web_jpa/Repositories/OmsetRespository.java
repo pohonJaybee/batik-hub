a4d52552d9bad4256fd4505dda5727f818fc71d0
Diamond Patrick <diamondpaatrick@gmail.com>
2026-06-13 03:14:47 +0700

    Remove @Repository because its not necessary

--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/OmsetRespository.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/OmsetRespository.java
@@ -4,12 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerce.web.jpa.e_commerce_web_jpa.Entities.Omset;
 
-@Repository
 public interface OmsetRespository extends JpaRepository<Omset, Integer> {
 
     @Modifying
--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/TransaksiRepository.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Repositories/TransaksiRepository.java
@@ -1,11 +1,9 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.ecommerce.web.jpa.e_commerce_web_jpa.Entities.Transaksi;
 
-@Repository
 public interface TransaksiRepository extends JpaRepository<Transaksi, Integer> {
 
 }
