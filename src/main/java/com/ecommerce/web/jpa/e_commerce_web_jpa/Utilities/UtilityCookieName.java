87130973a013ab962e331372c24b3facb930ffcc
Diamond Patrick <diamondpaatrick@gmail.com>
2026-06-13 00:00:33 +0700

    refactor code

--- src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Utilities/UtilityCookieName.java
+++ src/main/java/com/ecommerce/web/jpa/e_commerce_web_jpa/Utilities/UtilityCookieName.java
@@ -1,18 +1,15 @@
 package com.ecommerce.web.jpa.e_commerce_web_jpa.Utilities;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class UtilityCookieName {
 
     private static String id;
 
-    public static void setId(String idCookie) {
-        id = idCookie;
-    }
-
     public static String getId() {
         return id;
+    }
+
+    public static void setId(String idCookie) {
+        idCookie = id;
     }
 
 }
